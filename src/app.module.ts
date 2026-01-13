84dbaca32d6ba447c910c02f89bdb066bf12235e
hagay kohelet <hagay2506@gmail.com>
2026-01-13 22:15:07 +0200

    check the changes in app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule,SequelizeModule.forRootAsync({
+  useFactory: () => ({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    models: [User],
+  }),
+}),
+UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
